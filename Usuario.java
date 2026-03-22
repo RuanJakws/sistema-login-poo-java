83569fc8497340a06f2af40d39aad63e4899b615
Ruan Lumikowski <ruancardoso0911@gmail.com>
2026-03-22 15:12:07 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,4 +1,17 @@
-class Usuario {
+public class Usuario {
     private String email;
     private String senha;
+
+    public Usuario(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 }
